469caa5318dd5447c55321a776916fee8ace8445
Amritha Govindarajan <g.amritha@gmail.com>
2022-03-08 18:20:55 +0200

    4.4: helper functions and unit tests, step 2

--- utils/list_helper.js
+++ utils/list_helper.js
@@ -2,6 +2,17 @@ const dummy = (blogs) => {
   return blogs ? 1 : 1;
 };
 
+const totalLikes = (blogs) => {
+  const reducer = (previousValue, currentValue) => {
+    return previousValue + currentValue.likes;
+  };
+
+  return blogs.length === 0
+    ? 0
+    : blogs.reduce(reducer, 0);
+};
+
 module.exports = {
-  dummy
+  dummy,
+  totalLikes
 };
